9488e47366bd2893e423dbd9357e18314fe8de10
AleksandrGimanov <113610062+AleksandrGimanov@users.noreply.github.com>
2023-03-28 16:57:28 +0500

    ветка add-remove-endpoint

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -64,7 +64,8 @@ public class UserService {
 
         return userStorage.getUser(userId);
     }
-    public void deleteUser(int userId){
+
+    public void deleteUser(int userId) {
         if (!userStorage.isUserExistsById(userId)) {
             log.warn("Выполнена попытка получить пользователя по несущестующему id = {}", userId);
             throw new NotFoundException(String.format(Constants.USER_NOT_FOUND_MESSAGE, userId));
